4615ae78734ec8565c1fd637c5de9bce141b1af3
shubhampatel <sp246531@gmail.com>
2021-02-06 21:54:31 +0530

    added Continent and its test

--- src/main/java/entities/Continent.java
+++ src/main/java/entities/Continent.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 
@@ -7,6 +9,60 @@ package entities;
  *
  */
 public class Continent {
+	private String d_name;
+	private int d_controlValue;
+	private Set<Country> d_countriesSet;
+	
+	/**
+	 * Constructor for continent
+	 * @param p_name
+	 * @param p_controlValue
+	 */
+	public Continent(String p_name, int p_controlValue) {
+		d_name = p_name;
+		d_controlValue = p_controlValue;
+		d_countriesSet = new HashSet<Country>();
+	}
+
+	/**
+	 * Add country to the continent
+	 * @param p_country 
+	 */
+	public void addCountry(Country p_country) {
+		d_countriesSet.add(p_country);
+	}
+	
+	/**
+	 * Remove country from continent
+	 * @param p_country
+	 */
+	public void removeCountry(Country p_country) {
+		d_countriesSet.remove(p_country);
+	}
+	
+	/**
+	 * Get name of the continent
+	 * @return name of continent
+	 */
+	public String getName() {
+		return d_name;
+	}
+
+	/**
+	 * Get control value of the continent
+	 * @return control value of continent
+	 */
+	public int getControlValue() {
+		return d_controlValue;
+	}
+
+	/**
+	 * Get the set of countries in the continent
+	 * @return set of countries
+	 */
+	public Set<Country> getCountriesSet() {
+		return d_countriesSet;
+	}
 	
 	
 }
--- src/test/java/entities/ContinentTest.java
+++ src/test/java/entities/ContinentTest.java
@@ -4,11 +4,59 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Test for Continent
+ * @author shubhampatel
+ *
+ */
 public class ContinentTest {
 
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
+	Continent d_continent = new Continent("Asia", 3);
+	
+	/**
+	 * Check if continent object is not null
+	 */
+	@Test
+	public void checkNull() {
+		assertNotNull(d_continent);
+	}
+	
+	/**
+	 * Check name of continent
+	 */
+	@Test
+	public void nameTest() {
+		assertEquals("Asia", d_continent.getName());
+	}
+	
+	/**
+	 * Check control value of continent
+	 */
+	@Test
+	public void controlValueTest() {
+		assertEquals(3, d_continent.getControlValue());
+	}
 
+	/**
+	 * Check if countries are added
+	 */
+	@Test
+	public void addContriesTest() {
+		Country l_Country =  new Country("India", d_continent);
+		d_continent.addCountry(l_Country);
+		assertEquals(1, d_continent.getCountriesSet().size());
+		assertTrue(d_continent.getCountriesSet().contains(l_Country));
+	}
+	
+	/**
+	 * Check if countries are removed
+	 */
+	@Test
+	public void removeContriesTest() {
+		Country l_Country =  new Country("India", d_continent);
+		d_continent.addCountry(l_Country);
+		d_continent.removeCountry(l_Country);
+		assertEquals(0, d_continent.getCountriesSet().size());
+		assertFalse(d_continent.getCountriesSet().contains(l_Country));
+	}
 }
