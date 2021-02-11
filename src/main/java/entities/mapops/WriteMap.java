69fb892b89a1caf3a4e3087e372cf7ce26d9cf9c
Vandit <vanditthakkar09@gmail.com>
2021-02-11 16:57:14 +0530

    Update WriteMap.java

--- src/main/java/entities/mapops/WriteMap.java
+++ src/main/java/entities/mapops/WriteMap.java
@@ -1,8 +1,7 @@
 package entities.mapops;
-import java.io.BufferedWriter;
 import java.io.*;
-import java.util.HashMap;
-import java.lang.*;
+import java.util.*;
+import entities.*;
 
 /**
  * Class to write into map file
@@ -31,15 +30,16 @@ public class WriteMap{
 	 * 
 	 */
 	public boolean writeFullMap (String p_filePath) {
-		ClassLoader classLoader = Thread.currentThread().getContextLoader();
+//		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
 		//		File l_mapFile = new java.io.File(classLoader.getResource(p_filePath).getFile());
-		URL l_fileUrl = classLoader.getResource(p_filePath);
+//		Writer l_fileUrl = classLoader.getResource(p_filePath);
 		String l_line, l_dataString;
 		int l_countryCtn = 0, l_continentCtn = 0;
 
 		try {
-			d_writer = new BufferedWriter(l_mapFile);
+			FileWriter l_fw = new FileWriter(p_filePath);
+			d_writer = new BufferedWriter(l_fw);
 
 			//Writing Continents
 
@@ -47,51 +47,50 @@ public class WriteMap{
 			l_continents = d_gameMap.getContinents();
 			d_writer.write("[continents]");
 
-			for (Set<String> p_continents: l_continents.keySet()) {
-				d_continentsmap.put(p_continents, ++l_continentCtn);
+			for (String p_continents: l_continents.keySet()) {
+				d_continentsMap.put(p_continents, ++l_continentCtn);
 				d_writer.write(p_continents + " " + l_continents.get(p_continents).getControlValue());//is it a control value or the increment value
 				d_writer.newLine();
 			}
 
 
 			//Writing countries
-			l_continents = d_gameMap.getContinents();
 			d_writer.write("[countries]");
 
 			for (String p_continents: d_continentsMap.keySet()) {
 				Set<String> l_countriesName = l_continents.get(p_continents).getCountriesName();
+				
 
 				for(String p_countriesName: l_countriesName) {
-					d_countriesMap.put(p_countriesName, ++l_countriesCtn);
-					d.writer(l_countriesCtn + " " + p_countries + " " + d_continentsMap.get(p_continents));
+					d_countriesMap.put(p_countriesName, ++l_countryCtn);
+					d_writer.write(l_countryCtn + " " + p_countriesName + " " + d_continentsMap.get(p_continents));
 					d_writer.newLine();
 				}
 			}
 
+			
 			//Writing borders
-
 			HashMap<String, Country> l_countries = new HashMap<>();
 			l_countries = d_gameMap.getCountries();
 			d_writer.write("[borders]");
 
 			for (String p_countries: d_countriesMap.keySet()) {
-				Set<String> l_neighbourNames = new Set<>();
-				l_neighbourNames = l_countries.get(p_countries).getNeighbourNames;
-
+				Set<String> l_neighbourNames = l_countries.get(p_countries).getNeighbourNames();
+				
 				StringBuilder l_sb = new StringBuilder("");
 				l_sb.append(d_countriesMap.get(p_countries).toString() + " ");
 
 				for (String p_neighbourNames: l_neighbourNames) {
-					l_sb.append(d_countriesmap.get(p_neighbourNames).toString() + " ");
+					l_sb.append(d_countriesMap.get(p_neighbourNames).toString() + " ");
 				}
-				d_writer.write(l_sb);
+				d_writer.write(l_sb.toString());
 			}
 
-
-		} catch (Exception E) {
+		} catch (Exception e) {
 			System.out.println("Exception " + e.getMessage());
 			e.printStackTrace();
 		}
+		return true;
 	}
 
 }
