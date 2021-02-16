dbaf236136e02984dffa79162c355e528126ec7e
viragvora <virag.vora1999@gmail.com>
2021-02-16 14:49:42 +0530

    Update MapValidationTest.java

--- src/test/java/entities/MapValidationTest.java
+++ src/test/java/entities/MapValidationTest.java
@@ -6,19 +6,33 @@ import org.junit.Test;
 
 import entities.mapops.ReadMap;
 
+/**
+ * This class performs Unit testing on every function of the MapValidation Class
+ */
 public class MapValidationTest {
 
 	GameMap d_gameMap = new GameMap();
+	
+	/**
+	 * this will validate "isMapConnected" function with different maps provided in resource
+	 */
 	@Test
 	public void testIsMapConnected() {
 		d_gameMap.loadMap("WorldMap.txt");
-//		d_gameMap.loadMap("ameroki.map");
-//		d_gameMap.loadMap("risk.map");
+		
 		MapValidation l_mapValidation = new MapValidation(d_gameMap);
 		boolean l_testVar = l_mapValidation.isMapConnected(d_gameMap.getCountries().values().iterator().next() , d_gameMap.getCountries().keySet());
-		assertTrue(l_testVar);
+
+		assertFalse(l_testVar);
+		assertEquals(false, l_testVar);
 		
-		//fail("Not yet implemented");
+		d_gameMap.loadMap("risk.map");
+		l_mapValidation = new MapValidation(d_gameMap);
+		l_testVar = l_mapValidation.isMapConnected(d_gameMap.getCountries().values().iterator().next() , d_gameMap.getCountries().keySet());
+		
+		assertTrue(l_testVar);
+		assertEquals(true, l_testVar);
+		
 	}
 
 }
