f7dfbef221d04660455765f8a123daa79d1bd78c
shubhampatel <sp246531@gmail.com>
2021-02-19 21:39:28 +0530

    Update ReadMapTest.java
    added more test cases

--- src/test/java/entities/mapops/ReadMapTest.java
+++ src/test/java/entities/mapops/ReadMapTest.java
@@ -2,6 +2,7 @@ package entities.mapops;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -13,14 +14,32 @@ import entities.GameMap;
  */
 public class ReadMapTest {
 
+	ReadMap map;
+	
+	/**
+	 * Initialize map before each test
+	 */
+	@Before
+	public void initialize() {
+		map = new ReadMap(new GameMap());
+	}
+	
 	/**
 	 * Checks if map is read properly
 	 */
 	@Test
-	public void testReadFullMap() {
-		ReadMap map = new ReadMap(new GameMap());
+	public void testReadFullMap1() {
 		boolean l_result= map.readFullMap("uk.map");
 		assertTrue(l_result);
+	}
+	
+	/**
+	 * Check if missing map file results in false
+	 */
+	@Test
+	public void testReadFullMap2() {
+		boolean l_result= map.readFullMap("ukk.map");
+		assertFalse(l_result);
 	}
 }
 
