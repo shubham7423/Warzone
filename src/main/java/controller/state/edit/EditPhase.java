e08849876775e0223ae3c42a4072d402dac6dfc2
Jay Patel <pateljay2098@gmail.com>
2021-03-18 20:56:21 +0530

    javadoc for gamephase

--- src/main/java/controller/state/edit/EditPhase.java
+++ src/main/java/controller/state/edit/EditPhase.java
@@ -6,7 +6,6 @@ import controller.state.Phase;
 
 /**
  * EditPhase is inherited from the Phase class to support commands valid in editing phase
- *
  */
 public abstract class EditPhase extends Phase {
 	
@@ -59,6 +58,7 @@ public abstract class EditPhase extends Phase {
 	
 	/**
 	 * function to print invalid command as the following command cannot be used in this phase
+	 * @param p_playerName name of the player to add
 	 * @return string to print the invalid command message
 	 */
 	public String addPlayer(String p_playerName) {
@@ -67,6 +67,7 @@ public abstract class EditPhase extends Phase {
 	
 	/**
 	 * function to print invalid command as the following command cannot be used in this phase
+	 * @param p_playerName name of the player to remove
 	 * @return string to print the invalid command message
 	 */
 	public String removePlayer(String p_playerName) {
@@ -91,6 +92,7 @@ public abstract class EditPhase extends Phase {
 	
 	/**
 	 * function to print invalid command as the following command cannot be used in this phase
+	 * @param p_commandSplitted splitted command parts used for execution of command
 	 * @return string to print the invalid command message
 	 */
 	public String gamePlayer(String[] p_commandSplitted) {
@@ -99,15 +101,16 @@ public abstract class EditPhase extends Phase {
 	
 	/**
 	 * function to print invalid command as the following command cannot be used in this phase
+	 * @param p_fileName name of the map file used for loading
 	 * @return string to print the invalid command message
 	 */
 	public String loadMap(String p_fileName) {
 		return printInvalidCommandMessage();
-		
 	}
 	
 	/**
 	 * function to print invalid command as the following command cannot be used in this phase
+	 * @param p_commandSplitted splitted command parts used for execution of command
 	 * @return string to print the invalid command message
 	 */
 	public String deploy(String[] p_commandSplitted) {
