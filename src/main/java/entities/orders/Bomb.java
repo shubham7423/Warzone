9091dc00242527e214f943b255f6feb4cea1032b
viragvora <virag.vora1999@gmail.com>
2021-03-19 15:16:15 +0530

    created bomb.java starter file

---
+++ src/main/java/entities/orders/Bomb.java
@@ -0,0 +1,20 @@
+package entities.orders;
+
+import controller.GameEngine;
+
+/**
+ * This class represents the bomb card.
+ */
+public class Bomb implements Orders {
+
+	/**
+	 * @param p_game gets the object of GameEngine class
+	 * @return string
+	 */
+	@Override
+	public String executeOrder(GameEngine p_game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
