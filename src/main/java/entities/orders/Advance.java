17347419c0627ec0a85c8fcc3a9b1cee019f9e08
viragvora <virag.vora1999@gmail.com>
2021-03-19 15:14:10 +0530

    added errorless javadoc for advance command file

--- src/main/java/entities/orders/Advance.java
+++ src/main/java/entities/orders/Advance.java
@@ -2,8 +2,15 @@ package entities.orders;
 
 import controller.GameEngine;
 
+/**
+ * This class represents the advance command.
+ */
 public class Advance implements Orders {
 
+	/**
+	 * @param p_game gets the object of GameEngine class
+	 * @return string
+	 */
 	@Override
 	public String executeOrder(GameEngine p_game) {
 		// TODO Auto-generated method stub
