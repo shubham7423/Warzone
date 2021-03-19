6bb1a840335a2c6cdf7c76e8af80689a409d4bb7
viragvora <virag.vora1999@gmail.com>
2021-03-19 15:22:30 +0530

    created diplomacy.java starter file for diplomacy card

---
+++ src/main/java/entities/orders/Diplomacy.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package entities.orders;
+
+import controller.GameEngine;
+
+/**
+ * This class represents the diplomacy card.
+ */
+public class Diplomacy implements Orders {
+
+	/**
+	 * @param p_game gets the object of GameEngine class
+	 * @return string
+	 */
+	@Override
+	public String executeOrder(GameEngine p_game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
