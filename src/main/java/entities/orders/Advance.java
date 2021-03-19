95faee43e45dfb12db22ed39d1ebb3a281f8a550
viragvora <virag.vora1999@gmail.com>
2021-03-19 15:10:38 +0530

    Created Advance.java starter file

---
+++ src/main/java/entities/orders/Advance.java
@@ -0,0 +1,13 @@
+package entities.orders;
+
+import controller.GameEngine;
+
+public class Advance implements Orders {
+
+	@Override
+	public String executeOrder(GameEngine p_game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
