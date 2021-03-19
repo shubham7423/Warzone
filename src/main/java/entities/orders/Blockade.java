7bf99dc12c87d774144ae6039e6def084539adfe
viragvora <virag.vora1999@gmail.com>
2021-03-19 15:19:00 +0530

    created blockade.java starter file for blockade card

---
+++ src/main/java/entities/orders/Blockade.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package entities.orders;
+
+import controller.GameEngine;
+
+/**
+ * This class represents the blockade card.
+ */
+public class Blockade implements Orders {
+
+	/**
+	 * @param p_game gets the object of GameEngine class
+	 * @return string
+	 */
+	@Override
+	public String executeOrder(GameEngine p_game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
