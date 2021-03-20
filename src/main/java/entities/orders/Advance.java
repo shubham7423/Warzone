d021d60a3cd418b8de22fa98b0f2edbb9dfa0307
Vigilante7478 <vanditthakkar09@gmail.com>
2021-03-20 11:26:09 +0530

    Update Advance.java

--- src/main/java/entities/orders/Advance.java
+++ src/main/java/entities/orders/Advance.java
@@ -1,6 +1,7 @@
 package entities.orders;
 
 import controller.GameEngine;
+import entities.GameMap;
 import entities.Player;
 
 /**
@@ -8,9 +9,10 @@ import entities.Player;
  */
 public class Advance implements Orders {
 	
-	private Player d_Player;
-	private String d_countryNameFrom;
-	private String d_countryNameTo;
+	private GameMap d_gameMap; 
+	private Player d_player;
+	private int d_countryNameFrom;
+	private int d_countryNameTo;
 	private int d_armies;
 	
 	/**
@@ -21,11 +23,12 @@ public class Advance implements Orders {
 	 * @param p_countryNameTo country to which reinforcements are to be placed 
 	 * @param p_armies number of reinforcements to be placed
 	 */
-	public Advance(Player p_player, String p_countryNameFrom, String p_countryNameTo, int p_armies) {
-		d_Player = p_player;
+	public Advance(Player p_player, int p_countryNameFrom, int p_countryNameTo, int p_armies) {
+		d_player = p_player;
 		d_countryNameFrom = p_countryNameFrom;
 		d_countryNameTo = p_countryNameTo;
 		d_armies = p_armies;
+		d_gameMap = new GameMap();
 	}
 	/**
 	 * @param p_game gets the object of GameEngine class
@@ -34,6 +37,15 @@ public class Advance implements Orders {
 	@Override
 	public String executeOrder(GameEngine p_game) {
 		// TODO Auto-generated method stub
+		if ((d_gameMap.getCountries().get(d_countryNameFrom) == null) || (d_gameMap.getCountries().get(d_countryNameTo) == null)) {
+			return String.format("Country \"%d\" does not exist or country \"%d\" does not exist", d_countryNameFrom, d_countryNameTo);
+		}
+		else {
+			if (!d_player.getCountries().containsKey(d_countryNameFrom)) {
+				return String.format("Player \"%s\" does not control country \"%d\" ,hence armies cannot be moved.", d_player.getName(), d_countryNameFrom);
+			}
+		}
+		
 		return null;
 	}
 
