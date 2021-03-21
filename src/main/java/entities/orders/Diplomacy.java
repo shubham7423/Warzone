74554934fef7e9b8d78ad66976d974495002c6a2
viragvora <virag.vora1999@gmail.com>
2021-03-22 03:21:13 +0530

    updated diplomacy.java with javadoc

--- src/main/java/entities/orders/Diplomacy.java
+++ src/main/java/entities/orders/Diplomacy.java
@@ -7,38 +7,47 @@ import controller.GameEngine;
 import entities.Player;
 
 /**
- * This class represents the diplomacy card.
+ * This class represents the diplomacy card. The Diplomacy Card enforces peace
+ * between two players for a variable number of turns. While peace is enforced,
+ * neither player will be able to attack the other. The card takes effect the
+ * turn after it is played.
  */
 public class Diplomacy implements Orders {
 
 	private Player d_player;
 	private Player d_otherPlayer;
 	private String d_otherPlayerName;
-	
+
+	/**
+	 * Constructor of Diplomacy Card class for instantiating members.
+	 * @param p_player Player object of the player who wants to negotiate with other player for one time.
+	 * @param p_otherPlayer Name of the other player who is being negotiated for one time.
+	 */
 	public Diplomacy(Player p_player, String p_otherPlayer) {
 		d_player = p_player;
 		d_otherPlayerName = p_otherPlayer;
 	}
-	
+
 	/**
 	 * @param p_game gets the object of GameEngine class
-	 * @return string
+	 * @return string Result based on the execution of the Diplomacy Card.
 	 */
 	@Override
 	public String executeOrder(GameEngine p_game) {
 		// TODO Auto-generated method stub
-		if(!p_game.d_playerName.contains(d_otherPlayerName)) {
+		if (!p_game.d_playerName.contains(d_otherPlayerName)) {
 			return "Mentioned Player \"" + d_otherPlayerName + "\" does not exist";
 		} else {
 			int l_diplomacyCardCount = d_player.d_cardsOwned.get("diplomacy");
-			if(l_diplomacyCardCount < 1) {
+			if (l_diplomacyCardCount < 1) {
 				return "Player does not have Diplomacy Card.";
 			} else {
 				d_otherPlayer = p_game.d_players.get(d_otherPlayerName);
 				d_otherPlayer.d_negotiatedPlayerNames.add(d_player.getName());
 				d_player.d_negotiatedPlayerNames.add(d_otherPlayer.getName());
-				d_player.d_cardsOwned.replace("diplomacy", (l_diplomacyCardCount-1));
-				return "Negotiation between Players \"" + d_player.getName()+"\" and \"" + d_otherPlayer.getName() + "\" done successfully for 1 time.";
+				d_player.d_cardsOwned.replace("diplomacy", (l_diplomacyCardCount - 1));
+				return "Negotiation between Players \"" + d_player.getName() + "\" and \"" + d_otherPlayer.getName()
+						+ "\" done successfully for 1 time.";
 			}
 		}
 	}
