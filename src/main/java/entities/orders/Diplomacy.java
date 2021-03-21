1fc034834b404063e8d789da0202cdd248220948
viragvora <virag.vora1999@gmail.com>
2021-03-22 02:21:51 +0530

    initializing members in diplomacy.java

--- src/main/java/entities/orders/Diplomacy.java
+++ src/main/java/entities/orders/Diplomacy.java
@@ -4,12 +4,21 @@
 package entities.orders;
 
 import controller.GameEngine;
+import entities.Player;
 
 /**
  * This class represents the diplomacy card.
  */
 public class Diplomacy implements Orders {
 
+	private Player d_player;
+	private int d_otherPlayer;
+	
+	public Diplomacy(Player p_player, int p_otherPlayer) {
+		d_player = p_player;
+		d_otherPlayer = p_otherPlayer;
+	}
+	
 	/**
 	 * @param p_game gets the object of GameEngine class
 	 * @return string
