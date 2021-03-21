cb9aaf5b742b8499c64001df00788e8fcdaaa208
Meet Patel <meetpatel26722@gmail.com>
2021-03-22 00:03:58 +0530

    Update Blockade.java

--- src/main/java/entities/orders/Blockade.java
+++ src/main/java/entities/orders/Blockade.java
@@ -31,7 +31,7 @@ public class Blockade implements Orders {
 	@Override
 	public String executeOrder(GameEngine p_game) {
 		if(d_player.d_cardsOwned.get("blockade")>=1){
-			if(d_player.getCountries().containsKey(d_country){	
+			if(d_player.getCountries().containsKey(d_country)){	
 				int l_armiesPresence = d_player.getCountries().get(d_country).getNumberOfArmiesPresent();
 				if(l_armiesPresence > 0){
 					d_player.getCountries().get(d_country).setNumberOfArmiesPresent(l_armiesPresence*3);
