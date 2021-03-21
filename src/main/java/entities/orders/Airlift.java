7066b067e3ed43bf146eee841cec0ab1d638eb22
Vigilante7478 <vanditthakkar09@gmail.com>
2021-03-21 10:48:52 +0530

    Update Airlift.java

--- src/main/java/entities/orders/Airlift.java
+++ src/main/java/entities/orders/Airlift.java
@@ -4,12 +4,24 @@
 package entities.orders;
 
 import controller.GameEngine;
+import entities.Player;
 
 /**
  * This class represents the airlift card.
  */
 public class Airlift implements Orders {
 
+	private Player d_player;
+	private int d_countryNameFrom;
+	private int d_countryNameTo;
+	private int d_armies;
+	
+	public Airlift (Player p_player, int p_countryNameFrom, int p_countryNameTo, int p_armies) {
+		d_player = p_player;
+		d_countryNameFrom = p_countryNameFrom;
+		d_countryNameTo = p_countryNameTo;
+		d_armies = p_armies;
+	}
 	/**
 	 * @param p_game gets the object of GameEngine class
 	 * @return string
