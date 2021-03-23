123093fdf8aa00515fdcdc0ba280b9099a07b52a
Vigilante7478 <vanditthakkar09@gmail.com>
2021-03-23 16:08:21 +0530

    Create BlockadeTest.java

---
+++ src/test/java/entities/orders/BlockadeTest.java
@@ -0,0 +1,37 @@
+	package entities.orders;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import controller.GameEngine;
+import controller.state.gamephase.gamesetup.PostLoad;
+
+public class BlockadeTest {
+GameEngine d_game;
+	
+	@Before
+	public void setUp() {
+		d_game = new GameEngine();
+		d_game.setPhase(new PostLoad(d_game));
+		String[] l_newStrings = new String[] { "gameplayer", "-add", "Shubham", "-add", "Meet" };
+		d_game.executeCommand(l_newStrings);
+		d_game.getGameMap().addContinent(1, 5);
+		d_game.getGameMap().addCountry(1, 1);
+		d_game.getGameMap().addCountry(2, 1);
+		d_game.getGameMap().addNeighbor(1, 2);
+		d_game.getGameMap().getCountries().get(1).setPlayer(d_game.d_players.get("Shubham"));
+		d_game.getGameMap().getCountries().get(2).setPlayer(d_game.d_players.get("Meet"));
+		d_game.d_players.get("Shubham").addCountry(d_game.getGameMap().getCountries().get(1));
+		d_game.d_players.get("Shubham").setNumberOfArmies();
+		d_game.d_players.get("Meet").addCountry(d_game.getGameMap().getCountries().get(2));
+		d_game.d_players.get("Meet").setNumberOfArmies();
+		
+		Deploy l_deploy1 = new Deploy(d_game.d_players.get("Shubham"), 1, 2);
+		Deploy l_deploy2 = new Deploy(d_game.d_players.get("Meet"), 2, 3);
+		l_deploy1.executeOrder(d_game);
+		l_deploy2.executeOrder(d_game);
+	}
+}
