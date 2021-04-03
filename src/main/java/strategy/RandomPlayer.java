7e47b0c786a192e89f829cc40d9e40e3cc4f4e40
shubham patel <sp246531@gmail.com>
2021-04-03 15:40:17 +0530

    Update RandomPlayer.java

--- src/main/java/strategy/RandomPlayer.java
+++ src/main/java/strategy/RandomPlayer.java
@@ -3,8 +3,6 @@ package strategy;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 import controller.GameEngine;
 import entities.Country;
