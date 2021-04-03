71adcf0d529d96d1db0ee39427e32a5be41ae8c4
shubham patel <sp246531@gmail.com>
2021-04-03 15:36:45 +0530

    Update RandomPlayer.java

--- src/main/java/strategy/RandomPlayer.java
+++ src/main/java/strategy/RandomPlayer.java
@@ -10,6 +10,8 @@ import controller.GameEngine;
 import entities.Country;
 import entities.Player;
 import entities.orders.Advance;
+import entities.orders.Airlift;
+import entities.orders.Bomb;
 import entities.orders.Deploy;
 import entities.orders.Orders;
 
@@ -23,28 +25,48 @@ public class RandomPlayer extends PlayerStrategy{
 
 	public Orders createOrder() {
 		Orders l_order = null;
-		int l_randomOrder = d_gameEngine.d_random.nextInt() % 6;
+		int l_randomOrder = d_gameEngine.d_random.nextInt() % 5;
 		List<Integer> l_randCountries = new ArrayList<>(d_gameEngine.getGameMap().getCountries().keySet());
 		List<Integer> l_neighborCountries = new ArrayList<>(getNeighborCountries()); 
 		int l_randomArmies;
 		int l_randomOwnCountry;
 		int l_randomEnemyCountry;
 		switch (l_randomOrder) {
-		case 1:
+			case 1:
+				
+				l_randomOwnCountry = d_gameEngine.d_random.nextInt(l_randCountries.size());
+				l_randomArmies = d_gameEngine.d_random.nextInt(d_player.getNumberOfArmies());
+				l_order = new Deploy(d_player, l_randCountries.get(l_randomOwnCountry), l_randomArmies);
+				break;
+	
+			case 2:
+				l_randomOwnCountry = d_gameEngine.d_random.nextInt(l_randCountries.size());		
+				l_randomEnemyCountry = d_gameEngine.d_random.nextInt(l_neighborCountries.size());
+				l_randomArmies = d_gameEngine.d_random.nextInt(d_gameEngine.getGameMap().getCountries().get(l_randCountries.get(l_randomOwnCountry)).getNumberOfArmiesPresent() - 1);
+				l_order = new Advance(d_player, l_randCountries.get(l_randomOwnCountry), l_neighborCountries.get(l_randomEnemyCountry), l_randomArmies);
+				break;
+				
+			case 3:
+				l_randomOwnCountry = d_gameEngine.d_random.nextInt(l_randCountries.size());
+				int l_randomNeighbor = d_gameEngine.d_random.nextInt(d_gameEngine.getGameMap().getCountries().get(l_randomOwnCountry).getNeighborCountries().size());
+				int l_randomNeighborId = new ArrayList<>(d_gameEngine.getGameMap().getCountries().get(l_randomOwnCountry).getNeighborIds()).get(l_randomNeighbor);
+				l_randomArmies = d_gameEngine.d_random.nextInt(d_gameEngine.getGameMap().getCountries().get(l_randCountries.get(l_randomOwnCountry)).getNumberOfArmiesPresent() - 1);
+				l_order = new Advance(d_player, l_randCountries.get(l_randomOwnCountry), l_randomNeighborId, l_randomArmies);
+				break;
 			
-			l_randomOwnCountry = d_gameEngine.d_random.nextInt(l_randCountries.size());
-			l_randomArmies = d_gameEngine.d_random.nextInt(d_player.getNumberOfArmies());
-			l_order = new Deploy(d_player, l_randCountries.get(l_randomOwnCountry), l_randomArmies);
-			break;
-
-		case 2:
-			l_randomOwnCountry = d_gameEngine.d_random.nextInt(l_randCountries.size());		
-			l_randomEnemyCountry = d_gameEngine.d_random.nextInt(l_neighborCountries.size());
-			l_randomArmies = d_gameEngine.d_random.nextInt(d_gameEngine.getGameMap().getCountries().get(l_randCountries.get(l_randomOwnCountry)).getNumberOfArmiesPresent() - 1);
-			l_order = new Advance(d_player, l_randCountries.get(l_randomOwnCountry), l_neighborCountries.get(l_randomEnemyCountry), l_randomArmies);
+			case 4:
+				l_randomEnemyCountry = d_gameEngine.d_random.nextInt(l_neighborCountries.size());
+				l_order = new Bomb(d_player, l_randomEnemyCountry);
+				break;
 			
-		default:
-			break;
+			case 5:
+				l_randomOwnCountry = d_gameEngine.d_random.nextInt(l_randCountries.size());
+				l_randomArmies = d_gameEngine.d_random.nextInt(d_gameEngine.getGameMap().getCountries().get(l_randCountries.get(l_randomOwnCountry)).getNumberOfArmiesPresent() - 1);
+				l_order = new Airlift(d_player, l_randCountries.get(l_randomOwnCountry), l_randCountries.get(d_gameEngine.d_random.nextInt(l_randCountries.size())), l_randomArmies);
+				break;
+				
+			default:
+				break;
 		}
 		return l_order;
 	}
