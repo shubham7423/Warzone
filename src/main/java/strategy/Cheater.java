ff5d21c49e7e6b17c75e89b9349227844ef58600
viragvora <virag.vora1999@gmail.com>
2021-04-06 08:52:45 +0530

    Update Cheater.java

--- src/main/java/strategy/Cheater.java
+++ src/main/java/strategy/Cheater.java
@@ -5,18 +5,31 @@ import entities.Country;
 import entities.Player;
 import entities.orders.Dummy;
 import entities.orders.Orders;
+import java.util.*;
 
-public class Cheater extends PlayerStrategy{
-		
+/**
+ * This class has the behavior of the Cheater Player. All the neighbor countries
+ * are acquired by this Cheater Player and armies are doubled in those neighbor
+ * countries that are recently conquered.
+ */
+public class Cheater extends PlayerStrategy {
 	public Cheater(Player p_player, GameEngine p_gameEngine) {
 		super(p_player, p_gameEngine);
 	}
 
+	/**
+	 * Logic for the Cheater Player, acquiring all the neighbor countries and doubling the armies. 
+	 * return The dummy order is returned which which does not execute anything.
+	 */
 	@Override
 	public Orders createOrder() {
-		// TODO Auto-generated method stub
+		// get all the neighbors of the cheater player
+		Collection<Country> l_playerOwnedCountriesBefore = d_player.getCountries().values();
+		HashSet<Integer> l_playerOwnedCountriesAfter = new HashSet<>();
+		for (Country l_currentCountry : l_playerOwnedCountriesBefore) {
+			l_playerOwnedCountriesAfter.addAll(l_currentCountry.getNeighborIds());
+		}
+
 		return new Dummy();
 	}
-	
-
 }
