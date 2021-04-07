812dd229d21dd0da8fe47c08881d4ff4f2f8ee29
Jay Patel <pateljay2098@gmail.com>
2021-04-07 11:20:56 +0530

    javadoc for PLayerStrtegy

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"controller.state"},{"l":"controller.state.edit"},{"l":"controller.state.gamephase"},{"l":"controller.state.gamephase.endphase"},{"l":"controller.state.gamephase.gameplay"},{"l":"controller.state.gamephase.gamesetup"},{"l":"entities"},{"l":"entities.mapops"},{"l":"entities.orders"},{"l":"strategy"}]
