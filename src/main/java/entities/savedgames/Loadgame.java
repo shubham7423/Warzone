fcc74c40963cd474885141eaf1636c6a978281a1
viragvora <virag.vora1999@gmail.com>
2021-04-09 13:03:21 +0530

    loadgame constructor and loadGame method skeleton added

--- src/main/java/entities/savedgames/Loadgame.java
+++ src/main/java/entities/savedgames/Loadgame.java
@@ -1,5 +1,13 @@
 package entities.savedgames;
 
-public class Loadgame {
+import controller.GameEngine;
 
+public class Loadgame {
+	
+	public Loadgame(GameEngine p_GameEngine){	
+	}
+	
+	public String loadGame(String p_fileName){
+		return null;
+	}
 }
