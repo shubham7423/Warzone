c6ce4cded7927d7302958295b66d3e1f30e36577
Vigilante7478 <vanditthakkar09@gmail.com>
2021-04-10 16:35:22 +0530

    Update LoadGame.java

--- src/main/java/entities/savedgames/LoadGame.java
+++ src/main/java/entities/savedgames/LoadGame.java
@@ -17,13 +17,13 @@ public class LoadGame {
 	private HashMap<String, Integer> d_continentsMap;
 	private HashMap<Integer, String> d_countriesMap;
 	Scanner d_reader;
-	
-	public LoadGame(GameEngine p_gameEngine){
+
+	public LoadGame(GameEngine p_gameEngine) {
 		d_gameEngine = p_gameEngine;
 		d_continentsMap = new HashMap<>();
 		d_countriesMap = new HashMap<>();
 	}
-	
+
 	/**
 	 * Method that opens a file and reads the Game Data from that file.
 	 * 
@@ -37,7 +37,25 @@ public class LoadGame {
 			String l_line, l_dataString;
 			int l_countryCtn = 0, l_continentCtn = 0;
 			
-			
+			d_reader = new Scanner(l_mapFile);
+			while (d_reader.hasNextLine()) {
+				l_dataString = d_reader.nextLine();
+
+//				Read continents
+				if ("[continents]".equals(l_dataString)) {
+					while (d_reader.hasNextLine()) {
+						l_line = d_reader.nextLine();
+						if (l_line.length() > 0) {
+							String[] l_continents = l_line.split(" ");
+							++l_continentCtn;
+							d_continentsMap.put(l_continents[0], l_continentCtn);
+							d_gameEngine.getGameMap().addContinent(l_continentCtn, Integer.parseInt(l_continents[1]));
+						} else {
+							break;
+						}
+					}
+				}
+			}
 		}catch (FileNotFoundException p_e) {
 			System.out.println("Exception " + p_e.getMessage());
 			p_e.printStackTrace();
