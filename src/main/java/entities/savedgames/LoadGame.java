0906b1bae3ce9014e5037185bacd221f83b1d1ef
Vigilante7478 <vanditthakkar09@gmail.com>
2021-04-10 15:59:01 +0530

    Update LoadGame.java

--- src/main/java/entities/savedgames/LoadGame.java
+++ src/main/java/entities/savedgames/LoadGame.java
@@ -2,14 +2,25 @@ package entities.savedgames;
 
 import controller.GameEngine;
 
+/**
+ * Class to load a game from a ".game" file.
+ */
 public class LoadGame {
-	public GameEngine d_gameEngine;
+	private GameEngine d_gameEngine;
 	
 	public LoadGame(GameEngine p_gameEngine){
 		d_gameEngine = p_gameEngine;
 	}
 	
+	/**
+	 * Method that opens a file and reads the Game Data from that file.
+	 * 
+	 * @param p_fileName from .game file
+	 * @return 
+	 */
 	public String loadGame(String p_fileName){
+		
+		
 		return null;
 	}
 }
